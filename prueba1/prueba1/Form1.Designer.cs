adda88d3bb0a9af38b2d9d714e44ce103d1dd77d
tiniaAa <franciscovolovik@gmail.com>
2025-07-02 17:58:41 -0300

    guia 4

---
+++ prueba1/prueba1/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace prueba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bCalcular = new System.Windows.Forms.Button();
+            this.Ltexto = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bCalcular
+            // 
+            this.bCalcular.Location = new System.Drawing.Point(108, 359);
+            this.bCalcular.Name = "bCalcular";
+            this.bCalcular.Size = new System.Drawing.Size(142, 79);
+            this.bCalcular.TabIndex = 0;
+            this.bCalcular.Text = "calcular";
+            this.bCalcular.UseVisualStyleBackColor = true;
+            this.bCalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Ltexto
+            // 
+            this.Ltexto.AutoSize = true;
+            this.Ltexto.Location = new System.Drawing.Point(119, 40);
+            this.Ltexto.Name = "Ltexto";
+            this.Ltexto.Size = new System.Drawing.Size(140, 16);
+            this.Ltexto.TabIndex = 1;
+            this.Ltexto.Text = "Ingrese su peso en Kg";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = global::prueba1.Properties.Resources.download;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(64, 109);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(317, 244);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Ltexto);
+            this.Controls.Add(this.bCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bCalcular;
+        private System.Windows.Forms.Label Ltexto;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ prueba1/prueba1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prueba1
+{
+    public partial class Form1 : Form
+
+    {
+        private Image originalImage;
+        private float angle = 0f;
+        private Timer rotationTimer;
+
+        
+        private SoundPlayer oiegelda;
+        public Form1()
+        {
+            InitializeComponent();
+            oiegelda=new SoundPlayer(Properties.Resources.videoplayback);
+            // Cargar la imagen desde recursos (ajustá el nombre real del recurso)
+            originalImage = Properties.Resources.download;
+
+            // Configurar el PictureBox
+            pictureBox1.Paint += pictureBox1_Paint;
+            pictureBox1.BackColor = Color.Transparent;
+
+            // Inicializar el Timer
+            rotationTimer = new Timer();
+            rotationTimer.Interval = 20;
+            rotationTimer.Tick += RotationTimer_Tick;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            oiegelda.Play();
+            rotationTimer.Start();
+        }
+
+        private void RotationTimer_Tick(object sender, EventArgs e)
+        {
+            angle += 10f; // Ajustá la velocidad
+            if (angle >= 360f)
+                angle = 0f;
+
+            pictureBox1.Invalidate(); // Forzar redibujo
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (originalImage == null) return;
+
+            Graphics g = e.Graphics;
+            g.TranslateTransform(pictureBox1.Width / 2, pictureBox1.Height / 2);
+            g.RotateTransform(angle);
+            g.TranslateTransform(-originalImage.Width / 2, -originalImage.Height / 2);
+            g.DrawImage(originalImage, new Point(0, 0));
+        }
+
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
